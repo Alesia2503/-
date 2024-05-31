01994b0c14f011b0844c69fc61351bf6172a1ffb
Alesia_Shikurova <ales2503@yandex.by>
2024-05-31 12:59:50 +0300

    feat: add files

---
+++ main.js
@@ -0,0 +1,23 @@
+//Кнопка переход со ссылкой 
+document.querySelector('.btn_1').onclick = function(e) {
+  window.location = 'https://rolling-scopes-school.github.io/alesia2503-JSFE2023Q4/coffee-house/';
+};
+document.querySelector('.btn_4').onclick = function(e) {
+  window.location = 'https://fastmotors.by/';
+};
+document.querySelector('.btn_2').onclick = function(e) {
+  window.location = 'https://rolling-scopes-school.github.io/alesia2503-JSFE2023Q4/hangman/';
+};
+document.querySelector('.btn_3').onclick = function(e) {
+  window.location = 'https://rolling-scopes-school.github.io/alesia2503-JSFE2023Q4/nonograms/';
+};
+
+document.querySelector('.btn_5').onclick = function(e) {
+  window.location = 'https://alesia2503.github.io/site.2/';
+};
+document.querySelector('.btn_6').onclick = function(e) {
+  window.location = 'https://alesia2503.github.io/site3/';
+};
+document.querySelector('.btn_7').onclick = function(e) {
+  window.location = 'https://alesia2503.github.io/shop/';
+};
